eff98236def4cc8d3b3839041336a37d6aa3aecf
Rahulnsharma50 <djrahul8130565174@gmail.com>
2020-03-14 14:10:43 +0530

    Fixing errors

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const path = require('path');
 
-const http = require('http');
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
-const express = require('express');
 
 const app = express();
 
@@ -15,7 +13,7 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
@@ -24,4 +22,3 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 app.listen(3000);
-
